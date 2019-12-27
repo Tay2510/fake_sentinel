41800444623fa9453eda1a6e7457fd675ca7f722
tay2510 <tay2510@gmail.com>
2019-12-26 20:51:44 -0800

    Pre-pend source directory to the dataframe file path

--- fake_sentinel/data/query.py
+++ fake_sentinel/data/query.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from pathlib import Path
 
+SOURCE_DIR = '/home/jeremy/data/kaggle/dfdc/train/'
+
 METADATA_FILE = Path(__file__).parent / 'dfdc.csv'
 
 
-def load_dfdc_dataframe(metadata_file=METADATA_FILE):
+def load_dfdc_dataframe(metadata_file=METADATA_FILE, source_dir=SOURCE_DIR):
     df = pd.read_csv(metadata_file, index_col='index')
     df = clean_data(df)
+    df['filename'] = df['filename'].apply(lambda x: source_dir + x)
 
     return df
 
