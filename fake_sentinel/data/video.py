d51d7d1d185a11b38cdbb81d0ca871b3516e3a4c
Jeremy Yen <tay2510@gmail.com>
2019-12-27 19:59:05 -0800

    Added sampling interval when processing video frame

--- fake_sentinel/data/video.py
+++ fake_sentinel/data/video.py
@@ -3,17 +3,21 @@ import numpy as np
 from pathlib import Path
 
 
-def video_to_frames(video_path):
+def video_to_frames(video_path, sampling_interval=1):
     if not Path(video_path).is_file():
         raise FileNotFoundError
 
     vidcap = cv2.VideoCapture(str(video_path))
 
     frames = []
+    frame_count = 0
 
     while vidcap.grab():
-        _, frame = vidcap.retrieve()
-        frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        frame_count += 1
+
+        if frame_count % sampling_interval == 0:
+            _, frame = vidcap.retrieve()
+            frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     vidcap.release()
 
