2e9427a5d8871d7d5ceac1456d5c9ead71364c7d
Jeremy Yen <tay2510@gmail.com>
2019-12-28 17:05:41 -0800

    Added a simple jupyter video player for EDA

--- fake_sentinel/data/video.py
+++ fake_sentinel/data/video.py
@@ -1,6 +1,9 @@
+import io
+import base64
 import cv2
 import numpy as np
 from pathlib import Path
+from IPython.display import HTML
 
 
 def video_to_frames(video_path, sampling_interval=1):
@@ -22,3 +25,15 @@ def video_to_frames(video_path, sampling_interval=1):
     vidcap.release()
 
     return np.array(frames)
+
+
+def display_in_jupyter(video_path, width=880, height=495):
+    video = io.open(video_path, 'r+b').read()
+    encoded = base64.b64encode(video)
+
+    handle = HTML(data="""
+    <video  width={w} height={h} controls>
+        <source src="data:video/mp4;base64,{stream}" type="video/mp4" />
+    </video>""".format(w=width, h=height, stream=encoded.decode('ascii')))
+
+    return handle
