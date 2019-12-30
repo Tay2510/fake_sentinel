162eb4e6c81e537c980277f427f3b60d9069e154
Jeremy Yen <tay2510@gmail.com>
2019-12-29 17:29:00 -0800

    Added more video-related features for EDA purposes

--- fake_sentinel/data/video.py
+++ fake_sentinel/data/video.py
@@ -27,14 +27,33 @@ def video_to_frames(video_path, sampling_interval=1):
     return np.array(frames)
 
 
-def display_in_jupyter(video_path, width=880, height=495):
+def video_to_jupyter(video_path, width=704, height=396):
     if video_path:
         video = io.open(str(video_path), 'r+b').read()
         encoded = base64.b64encode(video)
 
         handle = HTML(data="""
-        <video  width={w} height={h} controls>
+        <video  width={w} height={h} controls muted>
             <source src="data:video/mp4;base64,{stream}" type="video/mp4" />
         </video>""".format(w=width, h=height, stream=encoded.decode('ascii')))
 
         return handle
+
+
+def join_videos(video_path_1, video_path_2, result_path):
+    video_1 = video_to_frames(video_path_1)
+    video_2 = video_to_frames(video_path_2)
+    result = np.concatenate((video_1, video_2), axis=2)
+
+    frames_to_video(result, result_path)
+
+
+def frames_to_video(frames, save_path, fps=30):
+    import imageio
+
+    writer = imageio.get_writer(save_path, fps=fps)
+
+    for f in frames:
+        writer.append_data(f)
+
+    writer.close()
