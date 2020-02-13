cf86d9b57024a691e969bbf36f2f0ed1782cb8bd
Jeremy Yen <tay2510@gmail.com>
2020-02-12 22:09:37 -0800

    Add an efficient video frame sampler

--- fake_sentinel/data/video.py
+++ fake_sentinel/data/video.py
@@ -32,6 +32,31 @@ def video_to_frames(video_path, sampling_interval=1, return_index=False):
         return np.array(frames)
 
 
+def sample_video_frames(video_path, sample_indices=[]):
+    if not Path(video_path).is_file():
+        raise FileNotFoundError
+
+    vidcap = cv2.VideoCapture(str(video_path))
+
+    frames = []
+
+    if len(sample_indices) > 0:
+        frame_index = -1
+        sample_index = 0
+
+        while vidcap.grab() and sample_index < len(sample_indices):
+            frame_index += 1
+
+            if frame_index == sample_indices[sample_index]:
+                _, frame = vidcap.retrieve()
+                frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                sample_index += 1
+
+        vidcap.release()
+
+    return np.array(frames)
+
+
 def video_to_jupyter(video_path, width=704, height=396):
     if video_path:
         video = io.open(str(video_path), 'r+b').read()
