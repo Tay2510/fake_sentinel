2a77b2267c954858e8f8159ede3fe5e167938ab9
Jeremy Yen <tay2510@gmail.com>
2020-02-15 12:43:08 -0800

    Some refactoring for the tracking package

---
+++ fake_sentinel/face/tracking/matcher.py
@@ -0,0 +1,117 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+from .box_utils import iou
+from .tracker import KalmanBoxTracker
+
+
+class Sort(object):
+    def __init__(self, max_age=1, min_hits=3, iou_threshold=0.3):
+        """
+        Sets key parameters for SORT
+        """
+        self.max_age = max_age
+        self.min_hits = min_hits
+        self.trackers = []
+        self.frame_count = 0
+        self.obj_count = 0
+        self.iou_threshold = iou_threshold
+
+    def update(self, dets):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections.
+        Returns the a similar array, where the last column is the object ID.
+        NOTE: The number of objects returned may differ from the number of detections provided.
+        """
+        self.frame_count += 1
+        # get predicted locations from existing trackers.
+        trks = np.zeros((len(self.trackers), 5))
+        to_del = []
+        ret = []
+
+        for t, trk in enumerate(trks):
+            pos = self.trackers[t].predict()[0]
+            trk[:] = [pos[0], pos[1], pos[2], pos[3], 0]
+            if (np.any(np.isnan(pos))):
+                to_del.append(t)
+
+        trks = np.ma.compress_rows(np.ma.masked_invalid(trks))
+
+        for t in reversed(to_del):
+            self.trackers.pop(t)
+        matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(dets, trks, iou_threshold=self.iou_threshold)
+
+        # update matched trackers with assigned detections
+        for t, trk in enumerate(self.trackers):
+            if (t not in unmatched_trks):
+                d = matched[np.where(matched[:, 1] == t)[0], 0]
+                trk.update(dets[d, :][0])
+
+        # create and initialise new trackers for unmatched detections
+        for i in unmatched_dets:
+            trk = KalmanBoxTracker(dets[i, :], self.obj_count)
+            self.trackers.append(trk)
+            self.obj_count += 1
+        i = len(self.trackers)
+
+        for trk in reversed(self.trackers):
+            d = trk.get_state()[0]
+            if ((trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits)):
+                ret.append(np.concatenate((d, [trk.id + 1])).reshape(1, -1))  # +1 as MOT benchmark requires positive
+            i -= 1
+            # remove dead tracklet
+            if (trk.time_since_update > self.max_age):
+                self.trackers.pop(i)
+
+        if (len(ret) > 0):
+            return np.concatenate(ret)
+
+        return np.empty((0, 5))
+
+
+def associate_detections_to_trackers(detections, trackers, iou_threshold=0.3):
+    """
+    Assigns detections to tracked object (both represented as bounding boxes)
+    Returns 3 lists of matches, unmatched_detections and unmatched_trackers
+    """
+    if (len(trackers) == 0):
+        return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
+
+    iou_matrix = np.zeros((len(detections), len(trackers)), dtype=np.float32)
+
+    for d, det in enumerate(detections):
+        for t, trk in enumerate(trackers):
+            iou_matrix[d, t] = iou(det, trk)
+
+    matched_indices = np.array(linear_sum_assignment(-iou_matrix)).transpose()
+
+    unmatched_detections = []
+
+    for d, det in enumerate(detections):
+        if (d not in matched_indices[:, 0]):
+            unmatched_detections.append(d)
+
+    unmatched_trackers = []
+
+    for t, trk in enumerate(trackers):
+        if (t not in matched_indices[:, 1]):
+            unmatched_trackers.append(t)
+
+    # filter out matched with low IOU
+    matches = []
+
+    for m in matched_indices:
+        if (iou_matrix[m[0], m[1]] < iou_threshold):
+            unmatched_detections.append(m[0])
+            unmatched_trackers.append(m[1])
+        else:
+            matches.append(m.reshape(1, 2))
+
+    if (len(matches) == 0):
+        matches = np.empty((0, 2), dtype=int)
+    else:
+        matches = np.concatenate(matches, axis=0)
+
+    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
