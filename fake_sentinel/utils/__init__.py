749adc6198d5ad41a4698b17a215c3417aaf8c13
Jeremy Yen <tay2510@gmail.com>
2020-02-17 00:57:43 -0800

    Add a  utility function for data parallelism

--- fake_sentinel/utils/__init__.py
+++ fake_sentinel/utils/__init__.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+from multiprocessing import Pool
 from pathlib import Path
 
 
@@ -17,3 +19,11 @@ def load_pickle(file_path):
 def chunks(l, batch_size):
     for i in range(0, len(l), batch_size):
         yield l[i : i + batch_size]
+
+
+def parallelize_dataframe(df, func, n_cores=4):
+    df_split = np.array_split(df, n_cores)
+    pool = Pool(n_cores)
+    pool.map(func, df_split)
+    pool.close()
+    pool.join()
