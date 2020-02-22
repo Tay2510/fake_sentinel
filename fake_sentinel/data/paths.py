ad6a47cbda639e18890d3869264238c711e96667
Jeremy Yen <tay2510@gmail.com>
2020-02-22 13:41:23 -0800

    Use symbolic link to organize pipeline datasets

--- fake_sentinel/data/paths.py
+++ fake_sentinel/data/paths.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 
+DATASET_DIR = Path(__file__).parents[2] / 'datasets'
+
 PREPROCESSED_FILE_DIR = Path(__file__).parent / 'resources'
+
 
 DFDC_DATAFRAME_FILE = PREPROCESSED_FILE_DIR / 'dfdc.csv'
 
-DFDC_TRAIN_VIDEO_DIR = '/home/jeremy/data/kaggle/dfdc/train/'
-
-FACENET_RESULT_DIR = '/home/jeremy/data/kaggle/dfdc_faces_100_frames'
-
-FACE_CROP_DIR = '/home/jeremy/data/kaggle/dfdc_face_crops'
-
 FACE_CROP_TABLE = PREPROCESSED_FILE_DIR / 'crop_table.pkl'
+
+
+# symbolic links to the datasets
+DFDC_TRAIN_VIDEO_DIR = DATASET_DIR / 'videos'
+
+FACENET_RESULT_DIR = DATASET_DIR / 'facenet_results'
+
+FACE_CROP_DIR = DATASET_DIR / 'face_crops'
