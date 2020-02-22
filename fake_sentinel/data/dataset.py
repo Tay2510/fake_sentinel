2435e1288fb7f13acd1e3017f8615aa2d9848a5b
Jeremy Yen <tay2510@gmail.com>
2020-02-22 11:56:22 -0800

    Add a starter Dataset class to sample face crops

---
+++ fake_sentinel/data/dataset.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+
+from fake_sentinel.data.sampler import CropSampler
+from fake_sentinel.data.image_utils import read_image
+
+LABEL_ENCODER = {
+    'FAKE': 1,
+    'REAL': 0
+}
+
+
+class FaceCropDataset(Dataset):
+    def __init__(self, dataframe):
+        self.sampler = CropSampler()
+        self.ids = dataframe.index.to_list()
+        self.labels = dataframe.label.to_list()
+        self.length = len(dataframe)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        sample_id, sample_label = self.ids[idx], self.labels[idx]
+        image_file = self.sampler.sample_from(sample_id)
+        image = read_image(image_file)
+        label = LABEL_ENCODER[sample_label]
+
+        return image, label
