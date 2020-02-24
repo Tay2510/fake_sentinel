bf5e0e7fd12635920dc0a10ecd28d2ad1e643046
Jeremy Yen <tay2510@gmail.com>
2020-02-23 16:52:46 -0800

    Organize the training pipeline and its configurations

--- fake_sentinel/pipeline/classifier_training_pipeline.py
+++ fake_sentinel/pipeline/classifier_training_pipeline.py
@@ -6,6 +6,7 @@ from fake_sentinel.data.query import load_crop_dataframe
 from fake_sentinel.data.loading.dataset import FaceCropDataset
 from fake_sentinel.model.classifier import create_classifier
 from fake_sentinel.train.helpers import train_model
+from fake_sentinel.pipeline.configs import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print('Using device:', device)
@@ -20,8 +21,8 @@ val_df = df[3000:3500]
 train_dataset = FaceCropDataset(train_df, 'train')
 val_dataset = FaceCropDataset(val_df, 'val')
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=BACKWARD_BATCH_SIZE, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=FORWARD_BATCH_SIZE, shuffle=False, num_workers=4)
 
 # Model
 print('Creating Model...')
@@ -32,7 +33,7 @@ summary(model, (3, 299, 299))
 
 criterion = torch.nn.CrossEntropyLoss()
 params_to_update = model.parameters()
-optimizer = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+optimizer = torch.optim.SGD(params_to_update, lr=INITIAL_LR, momentum=MOMENTUM)
 
 # Training
 print('Training...')
