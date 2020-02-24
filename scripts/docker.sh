79870bfbaf91268f64621f39b7112cc4ba24059a
Jeremy Yen <tay2510@gmail.com>
2020-02-23 16:02:24 -0800

    Increase docker SHM size for multi-process data loading

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,4 +2,4 @@
 
 DOCKER_IMAGE=$1
 
-docker run --gpus all -d --privileged --hostname docker -v $HOME:$HOME -e HOME=$HOME -e LOGNAME=$LOGNAME -p 8888:8888 -it $DOCKER_IMAGE
+docker run --gpus all -d --privileged --shm-size=1g --hostname docker -v $HOME:$HOME -e HOME=$HOME -e LOGNAME=$LOGNAME -p 8888:8888 -it $DOCKER_IMAGE
