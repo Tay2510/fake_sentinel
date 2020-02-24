17fc0f30a360787b35d00e772044b5cd7d384f7a
Jeremy Yen <tay2510@gmail.com>
2020-02-23 18:27:14 -0800

    Some printout formatting

--- fake_sentinel/pipeline/classifier_training_pipeline.py
+++ fake_sentinel/pipeline/classifier_training_pipeline.py
@@ -9,10 +9,10 @@ from fake_sentinel.train.helpers import train_model
 from fake_sentinel.pipeline.configs import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print('Using device:', device)
+print('\nUsing device:', device)
 
 # Data
-print('Loading Data...')
+print('\nLoading Data...')
 df = load_crop_dataframe()
 
 train_df = df[:3000]
@@ -25,7 +25,7 @@ train_loader = DataLoader(train_dataset, batch_size=BACKWARD_BATCH_SIZE, shuffle
 val_loader = DataLoader(val_dataset, batch_size=FORWARD_BATCH_SIZE, shuffle=False, num_workers=4)
 
 # Model
-print('Creating Model...')
+print('\nCreating Model...')
 model = create_classifier(pretrained=True)
 model.to(device)
 
@@ -36,6 +36,6 @@ params_to_update = model.parameters()
 optimizer = torch.optim.SGD(params_to_update, lr=INITIAL_LR, momentum=MOMENTUM)
 
 # Training
-print('Training...')
+print('\nTraining...')
 model, history = train_model(model=model, dataloaders={'train': train_loader, 'val': val_loader},
                              criterion=criterion, optimizer=optimizer, device=device)
