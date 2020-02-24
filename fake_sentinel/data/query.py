3aff3a8bee19ea8e2d3ce08f33d45094bbd8aeb9
Jeremy Yen <tay2510@gmail.com>
2020-02-23 23:55:28 -0800

    Add a function to split data into train/val

--- fake_sentinel/data/query.py
+++ fake_sentinel/data/query.py
@@ -29,6 +29,25 @@ def load_crop_dataframe(metadata_file=DFDC_DATAFRAME_FILE, crop_dir=FACE_CROP_DI
     return df
 
 
+def split_train_val(df, val_fraction=0.1):
+    test_originals = get_originals(df[df.split == 'val']).index.to_list()   # 200 REAL from public test
+    test_samples = df[df.original.isin(test_originals)].index.to_list()
+
+    train_originals = get_originals(df.loc[df.index.difference(test_samples)]).index.to_list()
+
+    cut_off = int(val_fraction * len(train_originals))
+
+    val_originals = train_originals[-cut_off:] + test_originals
+    train_originals = train_originals[:-cut_off]
+
+    df_train = df[df.original.isin(train_originals)]
+    df_val = df[df.original.isin(val_originals)]
+
+    assert len(df_train) + len(df_val) == len(df)
+
+    return df_train, df_val
+
+
 def replace_nan_with_id(df):
     original_ids = df[df.original.isnull()].original.index
     df.loc[original_ids, 'original'] = original_ids
