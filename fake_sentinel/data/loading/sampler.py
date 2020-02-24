d0fc1bd248b1cedc4418c1a7e900392b3458f704
Jeremy Yen <tay2510@gmail.com>
2020-02-23 16:43:01 -0800

    Make validation crop sampling become deterministic

--- fake_sentinel/data/loading/sampler.py
+++ fake_sentinel/data/loading/sampler.py
@@ -6,7 +6,9 @@ from fake_sentinel.data.paths import FACE_CROP_DIR, FACE_CROP_TABLE
 
 
 class CropSampler:
-    def __init__(self):
+    def __init__(self, mode='train'):
+        self.mode = mode
+
         with Path(FACE_CROP_TABLE).open('rb') as f:
             self.crop_table = pickle.load(f)
 
@@ -14,9 +16,14 @@ class CropSampler:
         face_crops = self.crop_table[sample_id]
         face_ids = list(face_crops.keys())
 
-        if len(face_ids) > 1:
+        if len(face_ids) > 1 and self.mode == 'train':
             face_id = random.choice(face_ids)
         else:
             face_id = face_ids[0]
 
-        return Path(FACE_CROP_DIR) / sample_id / str(face_id) / random.choice(face_crops[face_id])
+        if self.mode == 'train':
+            crop_path = random.choice(face_crops[face_id])
+        else:
+            crop_path = face_crops[face_id][0]
+
+        return Path(FACE_CROP_DIR) / sample_id / str(face_id) / crop_path
