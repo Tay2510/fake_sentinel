bd5695bbf5a56791119cd6629a99846074a26fc0
Jeremy Yen <tay2510@gmail.com>
2020-03-01 14:52:54 -0800

    Add versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+import fake_sentinel
+
 setup(
     name='fake_sentinel',
-    version='1.0.0',
-    description='Custom package for DFDC',
+    author='Jeremy Yen',
+    author_email='tay2510@gmail.com',
+    version=fake_sentinel.__version__,
+    description='Custom package for Kaggle DFDC competition',
     packages=find_packages(),
 )
