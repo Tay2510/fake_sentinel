8ad8fce6ca6f51a2a41b1edfc0cb32d6e5181d6a
Jeremy Yen <tay2510@gmail.com>
2020-03-18 12:54:47 -0700

    Use dictionary to organize configuration parameters

--- fake_sentinel/train/criteria.py
+++ fake_sentinel/train/criteria.py
@@ -1,11 +1,11 @@
 import torch
 
-from fake_sentinel.pipeline.configs import *
+from fake_sentinel.pipeline.configs import CONFIGS
 
 
 def get_criteria(model):
     params_to_update = model.parameters()
     criterion = torch.nn.functional.binary_cross_entropy_with_logits
-    optimizer = torch.optim.SGD(params_to_update, lr=INITIAL_LR, momentum=MOMENTUM)
+    optimizer = torch.optim.SGD(params_to_update, lr=CONFIGS['INITIAL_LR'], momentum=CONFIGS['MOMENTUM'])
 
     return criterion, optimizer
