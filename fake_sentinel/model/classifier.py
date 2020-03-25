d330de2a4405d3214e56f0f00f8e1813ec021d7b
Jeremy Yen <tay2510@gmail.com>
2020-03-24 20:02:06 -0700

    Generalize loading process of classifiers

--- fake_sentinel/model/classifier.py
+++ fake_sentinel/model/classifier.py
@@ -1,24 +1,34 @@
 import torch.nn as nn
+
 from fake_sentinel.model.cnn.xception import xception
 
 
-def create_classifier(pretrained=True, freeze_features=False):
+def create_classifier(model_name='xception', pretrained=True, freeze_features=False):
+    model = get_basic_model(model_name, pretrained=pretrained, freeze_features=freeze_features)
+
+    return model
+
+
+def get_basic_model(model_name, pretrained=True, freeze_features=False):
     if pretrained:
         pretrained_data = 'imagenet'
     else:
         pretrained_data = None
 
-    model = xception(num_classes=1000, pretrained=pretrained_data)
+    if model_name == 'xception':
+        model = xception(num_classes=1000, pretrained=pretrained_data)
 
-    if freeze_features:
-        for parameter in model.parameters():
-            parameter.requires_grad = False
+        if freeze_features:
+            for parameter in model.parameters():
+                parameter.requires_grad = False
 
-        for parameter in model.conv3.parameters():
-            parameter.requires_grad = True
+            for parameter in model.conv3.parameters():
+                parameter.requires_grad = True
 
-        for parameter in model.conv4.parameters():
-            parameter.requires_grad = True
+            for parameter in model.conv4.parameters():
+                parameter.requires_grad = True
+    else:
+        raise NotImplementedError
 
     # Handle the primary net
     num_features = model.last_linear.in_features
