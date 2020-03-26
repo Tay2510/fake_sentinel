3e6a1c010011711c5a2e436c585975f4e4769952
Jeremy Yen <tay2510@gmail.com>
2020-03-25 21:11:59 -0700

    Stop writing model architecture to the report because torchsummary is very unstable

--- fake_sentinel/report/notebook.py
+++ fake_sentinel/report/notebook.py
@@ -15,11 +15,8 @@ SECTION_TITLE = "# {title}"
 SECTION_CONFIGS_HEADER = "## Configurations"
 SECTION_CONFIGS_CODE = "display_configs()"
 
-SECTION_TRAINING_HEADER = "## Training History"
-SECTION_TRAINING_CODE = "display_training_curve(result_dir_path)"
-
 SECTION_EVALUATION_HEADER = "## Evaluation Result"
 SECTION_EVALUATION_CODE = "display_evaluation({eval_loss}, {eval_time})"
 
-SECTION_MODEL_HEADER = "## Model"
-SECTION_MODEL_CODE = "display_model()"
+SECTION_TRAINING_HEADER = "## Training History"
+SECTION_TRAINING_CODE = "display_training_curve(result_dir_path)"
