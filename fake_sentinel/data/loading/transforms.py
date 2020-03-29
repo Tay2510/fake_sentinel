a190ccc6dfd073316acf455597f03695b7ea2de1
Jeremy Yen <tay2510@gmail.com>
2020-03-28 21:04:50 -0700

    Make it easier to select model and input shapes

--- fake_sentinel/data/loading/transforms.py
+++ fake_sentinel/data/loading/transforms.py
@@ -1,11 +1,13 @@
 from torchvision import transforms
-from fake_sentinel.pipeline.configs import CONFIGS
+from fake_sentinel.pipeline.configs import CONFIGS, INPUT_SHAPES
+
+INPUT_SHAPE = INPUT_SHAPES[CONFIGS['MODEL_NAME']]
 
 
-def get_image_transforms(mode='train'):
+def get_image_transforms(mode='train', input_shape=INPUT_SHAPE):
     transform_sequence = [
         transforms.ToPILImage(),
-        transforms.Resize(CONFIGS['INPUT_SHAPE']),
+        transforms.Resize(input_shape),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
--- fake_sentinel/pipeline/configs.py
+++ fake_sentinel/pipeline/configs.py
@@ -9,8 +9,7 @@ CONFIGS = {
     'FORWARD_BATCH_SIZE': 128,
 
     # Model
-    'INPUT_SHAPE': (299, 299),  # 299*299 for Xception, 224*224 for ResNext
-    'MODEL_NAME': 'xception',  # 'xception' / 'resnext50' / 'resnext101'
+    'MODEL_NAME': 'xception',
     'PRETRAINED': True,
     'TRAIN_LOSS': 'BCE',  # 'BCE' / 'Focal'
 
@@ -20,8 +19,16 @@ CONFIGS = {
     'SMOOTHING_EPSILON': 0,
 
     # Training
+    'OPTIMIZER': 'SGD',  # 'SGD' / 'Adabound'
     'EPOCHS': 12,
     'INITIAL_LR': 0.004,
-    'OPTIMIZER': 'SGD',  # 'SGD' / 'Adabound'
     'MOMENTUM': 0.85
+}
+
+
+INPUT_SHAPES = {
+    'xception': (299, 299),
+    'hrnet': (256, 256),
+    'resnext50': (224, 224),
+    'resnext101': (224, 224)
 }
